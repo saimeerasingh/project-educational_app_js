e1b81793dc0bb3fb57ec5e51650867cdaeb8c769
saimeerasingh <singhsaimeera@gmail.com>
2022-01-24 23:58:41 +0000

    moved files to src

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,3 +1,23 @@
 use countries;
 
 db.dropDatabase();
+
+db.funfacts.insertMany(
+    [
+        {
+            country:"Hawaii",
+            funfact:"The best place in the world to see rainbows"
+        },
+        {
+            country:"China",
+            funfact:"All of China is on Beijing time despite geographically spanning over 5 time zones. "
+
+        },
+        {
+            country:"Hawaii",
+            funfact:"The best place in the world to see rainbows"
+        }
+
+    ]
+);
+
