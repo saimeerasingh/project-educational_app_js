15e22dcc7161f19ee72a30342b37589be8c86bc5
saimeerasingh <singhsaimeera@gmail.com>
2022-01-27 10:52:24 +0000

    more fun facts added to country

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -16,8 +16,23 @@ db.funfacts.insertMany(
         {
             country:"Hawaii",
             funfact:"The best place in the world to see rainbows"
+        },
+        {
+            country:"Portugal",
+            funfact:"Portugal is the oldest country in Europe"
+        },
+        {
+            country:"Germany",
+            funfact:"Oktoberfest actually begins in September"
+        },
+        {
+            country:"Sudan",
+            funfact:"Sudan has the most pyramids in the world (not Egypt)"
+        },
+        {
+            country:"Peru",
+            funfact:"Macchu Pichu is an earthquake-proof city"
         }
-
     ]
 );
 
